a0535eea88774940e84e696e88ae7dfcd97d5a3f
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-05-04 03:16:12 +0530

    06 project complete

--- 06-animations/exercise/src/script.js
+++ 06-animations/exercise/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,4 +30,26 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// // Clock
+// const clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
+// Animations
+const tick = () => {
+    // // Clock
+    // const elapsedTime = clock.getElapsedTime()
+
+    // // Update objects
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
