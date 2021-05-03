3179841decb0bd6997055128551a5cefa47e3621
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-05-03 23:29:30 +0530

    complete 3 & 4 exersize

--- 03-basic-scene/exercise/script.js
+++ 03-basic-scene/exercise/script.js
@@ -1,23 +1,22 @@
-import './style.css'
-import * as THREE from 'three'
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
     width: 800,
     height: 600
 }
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+})
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cubeMesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
